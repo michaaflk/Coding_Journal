13b9e7244963112b139e44d9d136603e7acf669e
michaaflk <michi-soad@gmx.net>
2023-01-25 14:03:00 +0100

    Dictionary pj added

---
+++ Dictionarys/Scrabble.py
@@ -0,0 +1,27 @@
+#Scrabble project by CodeCademy
+letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
+#Combine the lists above to a Dictionary
+letter_to_points = {let:poi for let, poi in zip(letters,points)}
+#Add key " " with value 0
+letter_to_points[" "] = 0
+
+def score_word(word):
+    point_total = 0
+    for letter in word:
+        point_total += letter_to_points.get(letter, 0)
+    return point_total
+
+brownie_points = score_word("BROWNIE")
+
+player_to_words = {"player1": ["BLUE", "TENNIS", "EXIT"], "wordNerd": ["EARTH", "EYES", "MACHINE"], "Lexi Con": ["ERASER", "BELLY", "HUSKY"], "Prof Reader": ["ZAP", "COMA", "PERIOD"]}
+
+player_to_points = {}
+
+for player, words in player_to_words.items():
+    player_points = 0
+    for word in words:
+        player_points += score_word(word)
+    player_to_points[player] = player_points
+    
+print(player_to_points)
